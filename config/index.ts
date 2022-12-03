d934e11db0d85a9bd396b28895386b9beaae6635
Arturo Yepez <arturyepez10@gmail.com>
2022-12-03 13:59:06 -0400

    feat: update envars config

--- config/index.ts
+++ config/index.ts
@@ -3,5 +3,11 @@ dotenv.config();
 
 export const config = {
   APP_URL: process.env.APP_URL ?? "",
-  APP_PORT: process.env.APP_PORT ?? 8000
+  APP_PORT: process.env.APP_PORT ?? 8000,
+  DATABASE_NAME: process.env.DATABASE_NAME ?? "",
+  DATABASE_USER: process.env.DATABASE_USER ?? "",
+  DATABASE_PASSWORD: process.env.DATABASE_PASSWORD ?? "",
+  DATABASE_HOST: process.env.DATABASE_HOST ?? "localhost",
+  DATABASE_PORT: process.env.DATABASE_PORT ? +process.env.DATABASE_PORT : 3306,
+  DATABASE_DIALECT: process.env.DATABASE_DIALECT ?? "mysql",
 }
