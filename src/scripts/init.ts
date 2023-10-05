1462831ba7c183f0b9f63a4ca07f7dcf6f2bad3e
Arturo Yepez <arturyepez10@gmail.com>
2023-10-06 01:10:46 +0200

    feat: added script that initializes the database

---
+++ src/scripts/init.ts
@@ -0,0 +1,19 @@
+import { eApplication } from "../classes";
+import { sequelize } from "../database";
+import { initConnection } from "../database/config";
+import { Application, Difficulty, HashingAlgorithm } from "../models";
+
+const initialization = async () => {
+  await initConnection();
+
+  // We add the 'bcrypt' initial Hashing Algorithm available in the database
+  await HashingAlgorithm.create({ name: 'bcrypt' });
+
+  // We add the initial applications available in the database
+  await Application.create({ name: eApplication.anar, description: "Main hub for ANAR applications." });
+  await Application.create({ name: eApplication.memory, description: "Memory game for cave figures." });
+
+  sequelize.close();
+};
+
+initialization();
