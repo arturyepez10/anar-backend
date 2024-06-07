935600fdd5bd2b4f73a7931112aa04b785ba7f5b
Arturo Yepez <arturyepez10@gmail.com>
2024-06-07 17:15:13 -0400

    feat: ability to create mock and test data into the db

---
+++ src/scripts/mock/data.ts
@@ -0,0 +1,8 @@
+
+export const testUserMock = {
+  username: "test_user",
+  first_name: "John",
+  last_name: "Doe",
+  password: "*Ab123456",
+  email: "test@test.com"
+}
---
+++ src/scripts/mock/functions.ts
@@ -0,0 +1,65 @@
+import bcrypt from 'bcryptjs';
+import { v4 as uuidv4 } from 'uuid';
+
+import { Email, UserAccount, UserAuthData } from "../../models";
+import { validatePassword } from "../../utils";
+import { testUserMock } from './data';
+
+export const createUserMock = async () => {
+  const { username, first_name, last_name, password, email } = testUserMock;
+
+  try {
+    // Check if email exists in the database
+    let bdEmail = await Email.findOne({ where: { address: email }, attributes: ['address', 'user_auth', 'user_id'] });
+
+    // If email exist and is associated with an account, return error
+    if (bdEmail && bdEmail.user_auth) {
+      throw new Error("Email already exist");
+    }
+
+    // We chek if the username is already taken
+    const authUser = await UserAuthData.findOne({ where: { username }, attributes: ['user_id', 'username', 'password_salt', 'password_hash', 'recovery_time', 'recovery_token', 'email', 'status', 'token_time', 'token_code', 'hash_algorithm', 'createdAt', 'updatedAt'] });
+    if (authUser) throw new Error("Username already exist");
+
+    // if email doesn't exist, create it
+    if (!bdEmail) bdEmail = await Email.create({ address: email });
+
+    // We validate the password
+    const passwordErrors = validatePassword(password);
+    if (passwordErrors.length > 0) throw new Error("Username already exist");
+
+    // We create a new user account and then associate it with the email
+    const userAuth = await UserAccount.create({
+      id: uuidv4(),
+      first_name,
+      last_name,
+    });
+
+    bdEmail.update({ user_auth: userAuth.id });
+
+    // We create the salt of the user and hash for the password
+    const password_salt = await bcrypt.genSalt();
+    const password_hash = await bcrypt.hash(password, password_salt);
+
+    // We create the user auth data
+    const authUserData = await UserAuthData.create({
+      user_id: userAuth.id,
+      username,
+      password_salt,
+      password_hash,
+      email: bdEmail.address,
+      status: 'pending',
+      hash_algorithm: 'Bcrypt'
+    });
+  } catch (error) {
+    // If something fails, we shold delete the user account and auth data}
+    await UserAccount.destroy({ where: { email } });
+    await UserAuthData.destroy({ where: { email } });
+
+    // Check if email exists in the database
+    let bdEmail = await Email.findOne({ where: { address: email }, attributes: ['address', 'user_auth', 'user_id'] });
+
+    // If email exist and is associated with an account, we deassociate it
+    if (bdEmail && bdEmail.user_auth) await Email.update({ user_auth: null }, { where: { address: email } });
+  }
+}
